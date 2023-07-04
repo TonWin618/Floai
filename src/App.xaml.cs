c4970b21d67022b514fb7a80effade5861031c75
汤问 <tonwin618@qq.com>
2023-07-05 03:04:36 +0800

    refactor: Start with Dependency Injection

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Floai.Utils.App;
+﻿using Floai.Pages;
+using Floai.Utils.App;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace Floai
@@ -8,10 +11,28 @@ namespace Floai
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
+        public static IHost? AppHost { get; private set; }
+        public App()
         {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddScoped<FloatView>();
+                }).Build();
+        }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+            var startup = AppHost.Services.GetRequiredService<FloatView>();
+            startup.Show();
+
             base.OnStartup(e);
             WindowsTaskbarIcon.Open();
+        }
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e);
         }
     }
 }
